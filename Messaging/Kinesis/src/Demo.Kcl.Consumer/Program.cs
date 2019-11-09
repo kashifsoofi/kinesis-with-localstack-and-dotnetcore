0a0664f769db2153a698b22dce8b96a653806afa
Kashif Jamal Soofi <kashif.soofi+git@gmail.com>
2019-11-09 14:36:08 +0000

    Kcl consumer (#3)
    * Create .dockerignore file
    
    * Added Bootstrapper and ClientLibrary from https://github.com/awslabs/amazon-kinesis-client-net
    
    Added DataMessageProcessor code from https://github.com/awslabs/amazon-kinesis-client-net
    
    * Added demo.kcl.consumer service to docker-compose
    
    Updated docker file, fixed casing for Kcl consumer project
    
    * Removed Kcl.Consumer directory
    
    * Readded directory to change case
    
    * Updated DataMessageProcessor to implement IShardRecordProcessor
    
    * Update kcl.properties
    
    * Updated dockerfiles to use runtime base image instead of aspnet image
    
    * Setup coredns to forward requests to google dns
    
    * Revert "Setup coredns to forward requests to google dns"
    
    This reverts commit f5f538a1ae2932f49dba85490d44c32d65fe2564.

---
+++ Messaging/Kinesis/src/Demo.Kcl.Consumer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Demo.Kcl.Consumer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var host = new HostBuilder()
+                .ConfigureServices((services) =>
+                {
+                    services.AddHostedService<MessageProcessorService>();
+                })
+                .UseConsoleLifetime()
+                .Build();
+
+            await host.RunAsync();
+        }
+    }
+}
