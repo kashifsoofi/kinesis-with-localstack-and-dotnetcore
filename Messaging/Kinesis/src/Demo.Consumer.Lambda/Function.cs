5095a070d83a593d57cb883314f1b7246118a7a8
Kashif Jamal Soofi <kashif.soofi+git@gmail.com>
2019-12-02 21:23:50 +0000

    Add Kinesis Lambda Consumer

---
+++ Messaging/Kinesis/src/Demo.Consumer.Lambda/Function.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.KinesisEvents;
+
+[assembly:LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+namespace Demo.Consumer.Lambda
+{
+
+    public class Function
+    {
+        public async Task Handler(KinesisEvent kinesisEvent, ILambdaContext context)
+        {
+            foreach (var record in kinesisEvent.Records)
+            {
+
+            }
+
+            await Task.CompletedTask;
+        }
+    }
+}
